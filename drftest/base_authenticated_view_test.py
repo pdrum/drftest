8a61aacaafbe0f0a79e85cf7b502219ae5f6a97e
pdrum <pedram.esmaeeli@gmail.com>
2018-11-18 03:46:59 +0330

    feat: Better documentation

--- drftest/base_authenticated_view_test.py
+++ drftest/base_authenticated_view_test.py
@@ -12,6 +12,12 @@ ABCTestMeta.add_ignored_test_class_name('BaseAuthenticatedViewTest')
 class BaseAuthenticatedViewTest(BaseViewTest, metaclass=ABCTestMeta):
     @abstractmethod
     def _get_permission_classes(self):
+        """
+        Should return a list of permission classes. drftest generates a testcase to check if
+        result of calling this method matches those specified by `permission_classes` attribute
+        of your class. This way you can test your permission classes separately and in other
+        tests just use drftest's auto-generated testcases to check if correct permissions are set.
+        """
         raise NotImplementedError()
 
     def _get_permission_classes_for_docs(self):
