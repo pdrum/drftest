c6610ce5c4cfe9de399a0e0b92eb6fb4977e5aa8
pdrum <pedram.esmaeeli@gmail.com>
2018-12-17 14:17:46 +0330

    test: Add tests for docs that are generated

--- drftest/doc_generator.py
+++ drftest/doc_generator.py
@@ -111,10 +111,10 @@ def write_docs():
         index_file.write("""
 # Welcome
 
-Welcome to *DRF Test* documentation. 
+Welcome to *DRF Test* documentation.
 
 This documentation is categorized according to subsystems of your code.
-Name of each subsystem is given in the navigation menu. You can click on 
+Name of each subsystem is given in the navigation menu. You can click on
 each item to visit documentation of that subsystem.
 
 Happy coding :)
---
+++ drftest/tests/test_settings.py
@@ -0,0 +1,33 @@
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'drftest',
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
+SECRET_KEY = "Dummy"
+TEST_RUNNER = 'drftest.TestRunner'
+DRF_TEST_AUTH_PROVIDER_CLASS = 'drftest.token_auth_provider.TokenAuthProvider'
+
+drftest_LISTENER_HOST = 'http://example.com'
+drftest_NAMESPACE = 'drftest'
